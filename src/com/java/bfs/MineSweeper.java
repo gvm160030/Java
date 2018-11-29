e5c86e8d2bd17bb9ed6272f356088d69cef5c070
Gauri Mutha <gvm160030@utdallas.edu>
2018-11-28 21:28:15 -0600

    BFS practice problems

--- src/com/java/bfs/MineSweeper.java
+++ src/com/java/bfs/MineSweeper.java
@@ -1,4 +1,63 @@
 package com.java.bfs;
 
+import java.util.Arrays;
+
 public class MineSweeper {
+    static int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}, {-1,-1}, {-1, 1},{1,-1}, {1,1}};
+    public static char[][] updateBoard(char[][] board, int[] click) {
+
+        if(board[click[0]][click[1]] == 'M')
+        {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+
+        bfs(board, click[0], click[1]);
+        return board;
+    }
+
+    static void bfs(char[][] board, int i, int j)
+    {
+        if((board[i][j] <= '8' && board[i][j] >= '1') || board[i][j] == 'B') {
+            return;
+        }
+
+        board[i][j] = 'B';
+
+        int mines = 0;
+        for(int[] dir: dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+
+            if(x<0||y<0||x>=board.length||y>=board[0].length)
+                continue;
+
+            if(board[x][y] == 'M')
+                mines++;
+        }
+
+        if(mines>0)
+        {
+            System.out.println("i : "+i+" and j : "+j+" | "+mines);
+            board[i][j] = (char) (mines + 48);
+            return;
+        }
+
+        for(int[] dir: dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+
+            if(x<0||y<0||x>=board.length||y>=board[0].length || board[x][y] != 'E')
+                continue;
+
+            bfs(board, x, y);
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {{'E','E','E','E','E'},{'E','E','M','E','E'},{'E','E','E','E','E'},{'E','E','E','E','E'}};
+        int[] click = {3,0};
+        System.out.println(Arrays.deepToString(updateBoard(board,click)));
+    }
+
 }
--- src/com/java/bfs/OpenLock.java
+++ src/com/java/bfs/OpenLock.java
@@ -1,4 +1,58 @@
 package com.java.bfs;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class OpenLock {
+    public static int openLock(String[] deadends, String target) {
+        Set<String> begin = new HashSet<>();
+        Set<String> end = new HashSet<>();
+        Set deads = new HashSet<>(Arrays.asList(deadends));
+        begin.add("0000");
+        end.add(target);
+        int level = 0;
+        Set temp;
+        while(!begin.isEmpty() && !end.isEmpty()) {
+            if (begin.size() > end.size()) {
+                temp = begin;
+                begin = end;
+                end = temp;
+            }
+            temp = new HashSet<>();
+            for(String s : begin) {
+                System.out.println("***************Begin size : "+begin.size());
+                if(end.contains(s)) return level;
+                if(deads.contains(s)) continue;
+                deads.add(s);
+                StringBuilder sb = new StringBuilder(s);
+                for(int i = 0; i < 4; i ++) {
+                    char c = sb.charAt(i);
+                    String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+                    String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+
+                    //Check if the answer has been found
+
+                    if(end.contains(s1) || end.contains(s2))
+                        return level+1;
+
+                    if(!deads.contains(s1))
+                        temp.add(s1);
+                    if(!deads.contains(s2))
+                        temp.add(s2);
+
+                }
+            }
+            System.out.println("level : "+level);
+            level ++;
+            begin = temp;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String[] deadends = {"0201","0101","0102","1212","2002"};
+        String target = "0202";
+        System.out.println(openLock(deadends,target));
+    }
 }
