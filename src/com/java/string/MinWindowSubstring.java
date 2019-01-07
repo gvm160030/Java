8ccfa7c4d0466e735a8a7df8c4d840dcad9a1110
Gauri Mutha <gvm160030@utdallas.edu>
2019-01-06 19:41:50 -0600

    Modification of the code

--- src/com/java/string/MinWindowSubstring.java
+++ src/com/java/string/MinWindowSubstring.java
@@ -1,4 +1,77 @@
 package com.java.string;
 
+import java.util.HashMap;
+/*
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+Example:
+
+Input: S = "ADOBECODEBANC", T = "ABC"
+Output: "BANC"
+Note:
+
+If there is no such window in S that covers all characters in T, return the empty string "".
+If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
+ */
 public class MinWindowSubstring {
+    public static String minWindow(String s, String t) {
+        if(t.length()>s.length())
+            return "";
+        String result = "";
+
+        //character counter for t
+        HashMap<Character, Integer> target = new HashMap<Character, Integer>();
+        for(int i=0; i<t.length(); i++){
+            char c = t.charAt(i);
+            if(target.containsKey(c)){
+                target.put(c,target.get(c)+1);
+            }else{
+                target.put(c,1);
+            }
+        }
+
+        // character counter for s
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int left = 0;
+        int minLen = s.length()+1;
+
+        int count = 0; // the total of mapped characters
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(target.containsKey(c)){
+                if(map.containsKey(c)){
+                    if(map.get(c)<target.get(c)){
+                        count++;
+                    }
+                    map.put(c,map.get(c)+1);
+                }else{
+                    map.put(c,1);
+                    count++;
+                }
+            }
+
+            if(count == t.length()){
+                char sc = s.charAt(left);
+                while (!map.containsKey(sc) || map.get(sc) > target.get(sc)) {
+                    if (map.containsKey(sc) && map.get(sc) > target.get(sc))
+                        map.put(sc, map.get(sc) - 1);
+                    left++;
+                    sc = s.charAt(left);
+                }
+
+                if (i - left + 1 < minLen) {
+                    result = s.substring(left, i + 1);
+                    minLen = i - left + 1;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minWindow("ADOBECODEBANC","ABC"));
+    }
 }
