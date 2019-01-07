565b6f42a3b3f21ac9db93a03973764eb1a1475b
Gauri Mutha <gvm160030@utdallas.edu>
2019-01-07 14:29:24 -0600

    Modified version

--- src/com/java/string/LongestSubstringNonRepeat.java
+++ src/com/java/string/LongestSubstringNonRepeat.java
@@ -1,4 +1,32 @@
 package com.java.string;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstringNonRepeat {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
+        Set<Character> chars = new HashSet<>();
+        int max = 1, head = 0, tail = 0;
+
+        while (head < s.length()) {
+            char currChar = s.charAt(head);
+
+            if (!chars.contains(currChar)) {
+                chars.add(currChar);
+                max = Math.max(max, chars.size());
+                head++;
+            } else {
+                chars.remove(s.charAt(tail));
+                tail++;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcbb"));
+    }
 }
