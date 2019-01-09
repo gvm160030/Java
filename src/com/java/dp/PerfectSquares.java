aae791e75bea407b90efce848b6d8808c5a9eaf9
Gauri Mutha <gvm160030@utdallas.edu>
2019-01-09 16:28:48 -0600

    Edited version

--- src/com/java/dp/PerfectSquares.java
+++ src/com/java/dp/PerfectSquares.java
@@ -1,4 +1,39 @@
 package com.java.dp;
 
+import java.util.Arrays;
+/*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+ */
 public class PerfectSquares {
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <= n; ++i) {
+            int min = Integer.MAX_VALUE;
+            int j = 1;
+            while(i - j*j >= 0) {
+                min = Math.min(min, dp[i - j*j] + 1);
+                ++j;
+            }
+            System.out.println("i : "+i+" min : "+min);
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numSquares(15));
+    }
 }
