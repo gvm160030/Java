25f206ba3743bd33d8dde226c5316c18adf8715b
Gauri Mutha <gvm160030@utdallas.edu>
2019-01-27 21:18:40 -0600

    Sorting stack

---
+++ src/com/java/stacks_queues/SortStack.java
@@ -0,0 +1,75 @@
+package com.java.stacks_queues;
+
+import java.util.Stack;
+
+/*
+Given a stack of integers, sort it in ascending order using another temporary stack
+
+Input : [34, 3, 31, 98, 92, 23]
+Output : [3, 23, 31, 34, 92, 98]
+
+Input : [3, 5, 1, 4, 2, 8]
+Output : [1, 2, 3, 4, 5, 8]
+ */
+public class SortStack {
+
+    /*
+    APPROACH
+
+    We follow this algorithm.
+
+Create a temporary stack say tmpStack.
+While input stack is NOT empty do this:
+Pop an element from input stack call it temp
+while temporary stack is NOT empty and top of temporary stack is greater than temp,
+pop from temporary stack and push it to the input stack
+push temp in temporary stack
+The sorted numbers are in tmpStack
+     */
+
+    public static Stack<Integer> sortstack(Stack<Integer>
+                                                   input)
+    {
+        Stack<Integer> tmpStack = new Stack<Integer>();
+        while(!input.isEmpty())
+        {
+            // pop out the first element
+            int tmp = input.pop();
+
+            // while temporary stack is not empty and
+            // top of stack is greater than temp
+            while(!tmpStack.isEmpty() && tmpStack.peek()
+                    > tmp)
+            {
+                // pop from temporary stack and
+                // push it to the input stack
+                input.push(tmpStack.pop());
+            }
+
+            // push temp in tempory of stack
+            tmpStack.push(tmp);
+        }
+        return tmpStack;
+    }
+
+    // Driver Code
+    public static void main(String args[])
+    {
+        Stack<Integer> input = new Stack<Integer>();
+        input.add(34);
+        input.add(3);
+        input.add(31);
+        input.add(98);
+        input.add(92);
+        input.add(23);
+
+        // This is the temporary stack
+        Stack<Integer> tmpStack=sortstack(input);
+        System.out.println("Sorted numbers are:");
+
+        while (!tmpStack.empty())
+        {
+            System.out.print(tmpStack.pop()+" ");
+        }
+    }
+}
