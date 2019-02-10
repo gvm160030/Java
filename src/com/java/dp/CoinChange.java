4ef464f7a18aab01a159c4ff4e2294e77af0b2a7
Gauri Mutha <gvm160030@utdallas.edu>
2019-02-10 15:24:46 -0600

    Modified version

--- src/com/java/dp/CoinChange.java
+++ src/com/java/dp/CoinChange.java
@@ -2,12 +2,12 @@ package com.java.dp;
 
 public class CoinChange {
 
-    static int countWays( int S[], int m, int n )
+    static int countWays( int S[], int coins, int sum )
     {
         // table[i] will be storing the number of solutions for
         // value i. We need n+1 rows as the table is constructed
         // in bottom up manner using the base case (n = 0)
-        int table[]=new int[n+1];
+        int table[]=new int[sum+1];
 
         // Base case (If given value is 0)
         table[0] = 1;
@@ -15,11 +15,11 @@ public class CoinChange {
         // Pick all coins one by one and update the table[] values
         // after the index greater than or equal to the value of the
         // picked coin
-        for(int i=0; i<m; i++)
-            for(int j=S[i]; j<=n; j++)
+        for(int i=0; i<coins; i++)
+            for(int j=S[i]; j<=sum; j++)
                 table[j] += table[j-S[i]];
 
-        return table[n];
+        return table[sum];
     }
 
     public static void main(String args[])
